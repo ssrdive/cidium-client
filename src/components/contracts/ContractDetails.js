60c3571031c2aa7dc217b8ba66e72478db618769
Shamal Sandeep <ssdh.gdrive@gmail.com>
2020-01-13 15:43:03 +0530

    Legacy receipt complete

--- src/components/contracts/ContractDetails.js
+++ src/components/contracts/ContractDetails.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Row, Badge, Col, Card, CardBody, Spinner } from 'reactstrap';
+import { Row, Badge, Col, Card, CardBody, Spinner } from 'reactstrap';
 
 import { apiAuth } from '../../cidium-api';
 
--- src/pages/contracts/payments.js
+++ src/pages/contracts/payments.js
@@ -135,6 +135,112 @@ const ReceiptForm = ({ valid, id }) => {
     );
 };
 
+const LegacyReceiptForm = ({ valid, id }) => {
+    const [loading, setLoading] = useState(false);
+    const [submitStatus, setSubmitStatus] = useState({ status: null, message: '' });
+    const [form, setForm] = useState({
+        amount: NUMBER_INPUT_REQUIRED,
+        notes: TEXTAREA_INPUT_OPTIONAL,
+    });
+
+    const handleOnChange = e => {
+        e.persist();
+        setForm(prevForm => {
+            const updatedForm = { ...prevForm, [e.target.name]: { ...prevForm[e.target.name] } };
+            updatedForm[e.target.name].value = e.target.value;
+            return updatedForm;
+        });
+    };
+    const handleFormSubmit = e => {
+        setLoading(prevLoading => true);
+        setSubmitStatus({ status: null, message: '' });
+        e.persist();
+        e.preventDefault();
+        if (!valid) return;
+        apiAuth
+            .post(
+                '/contract/receipt/legacy',
+                qs.stringify({
+                    cid: id,
+                    amount: form.amount.value,
+                    notes: form.notes.value,
+                    user_id: getLoggedInUser().id,
+                })
+            )
+            .then(response => {
+                setLoading(prevLoading => false);
+                setSubmitStatus({ status: 'success', message: `Receipt issued with number ${response.data}` });
+                // setTimeout(() => {
+                //     history.push(`/contracts/work/${response.data}`);
+                // }, 1000);
+            })
+            .catch(err => {
+                setLoading(prevLoading => false);
+                setSubmitStatus({ status: 'failure', message: 'Something went wrong' });
+            });
+    };
+
+    const SubmitComponent = () => {
+        return (
+            <>
+                {submitStatus.status !== null ? (
+                    submitStatus.status === 'success' ? (
+                        <UncontrolledAlert color="success">{submitStatus.message}</UncontrolledAlert>
+                    ) : (
+                        <UncontrolledAlert color="warning">{submitStatus.message}</UncontrolledAlert>
+                    )
+                ) : null}
+                {loading ? (
+                    <Spinner className="m-2" type="grow" color="success" />
+                ) : (
+                    <Button color="success" type="submit">
+                        Issue Receipt
+                    </Button>
+                )}
+            </>
+        );
+    };
+
+    return (
+        <Card>
+            <CardBody>
+                <h4 className="header-title mt-0">Create Legacy Receipt</h4>
+                <Row>
+                    <Col>
+                        {valid ? (
+                            <>
+                                <Row>
+                                    <Col md={12}>
+                                        <Form onSubmit={handleFormSubmit}>
+                                            <FormGroup>
+                                                <FormInput
+                                                    {...form['amount']}
+                                                    name="amount"
+                                                    placeholder="Amount"
+                                                    handleOnChange={handleOnChange}
+                                                />
+                                            </FormGroup>
+                                            <FormGroup>
+                                                <FormInput
+                                                    {...form['notes']}
+                                                    name="notes"
+                                                    placeholder="Notes"
+                                                    handleOnChange={handleOnChange}
+                                                />
+                                            </FormGroup>
+                                            <SubmitComponent />
+                                        </Form>
+                                    </Col>
+                                </Row>
+                            </>
+                        ) : null}
+                    </Col>
+                </Row>
+            </CardBody>
+        </Card>
+    );
+};
+
 const SelectContractForm = ({ setID }) => {
     const [form, setForm] = useState({
         id: NUMBER_INPUT_REQUIRED,
@@ -231,6 +337,9 @@ export default ({ location }) => {
             <Row>
                 <Col md={4}>
                     <DebitNote />
+                </Col>
+                <Col md={4}>
+                    <LegacyReceiptForm id={id} valid={valid} />
                 </Col>
             </Row>
         </React.Fragment>
