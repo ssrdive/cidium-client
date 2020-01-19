b2512f6616a901fd6a9cb23cab56baac193130bb
Shamal Sandeep <ssdh.gdrive@gmail.com>
2020-01-19 17:52:53 +0530

    API modification

--- src/cidium-api.js
+++ src/cidium-api.js
@@ -2,13 +2,21 @@ import axios from 'axios';
 import { getLoggedInUser } from './helpers/authUtils'
 
 const token = getLoggedInUser() != null ? getLoggedInUser().token : null
+let baseURL
+if(process.env.NODE_ENV === 'development') {
+    baseURL = 'http://localhost:4000'
+  }
+  
+  if(process.env.NODE_ENV === 'production') {
+    baseURL = 'https://agrivest.app/api'
+  }
 
 export default axios.create({
-    baseURL: 'http://localhost:4000'
+    baseURL
 });
 
 export const apiAuth = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL,
     headers: {
         Authorization: `Bearer ${token}`
     }
