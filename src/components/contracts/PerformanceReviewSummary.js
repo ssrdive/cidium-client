5ad67cf58245e18a5f8737a7c3096390c3082cb6
Shamal Sandeep <ssdh.gdrive@gmail.com>
2020-05-31 12:51:32 +0530

    In between performance review

--- src/components/contracts/PerformanceReviewSummary.js
+++ src/components/contracts/PerformanceReviewSummary.js
@@ -12,6 +12,8 @@ export default ({ results, loading, startdate, enddate }) => {
         let totalOverdue = 0;
         let totalStartOverdue = 0;
         let totalEndOverdue = 0;
+        let totalStartBetweenOverdue = 0;
+        let totalEndBetweenOverdue = 0;
         let startOdIndex = 0;
         let endOdIndex = 0;
         let totalAgreement = 0;
@@ -22,6 +24,8 @@ export default ({ results, loading, startdate, enddate }) => {
             totalOverdue += parseFloat(results[i].amount_pending);
             totalStartOverdue += parseFloat(results[i].start_amount_pending);
             totalEndOverdue += parseFloat(results[i].end_amount_pending);
+            totalStartBetweenOverdue += parseFloat(results[i].start_between_amount_pending);
+            totalEndBetweenOverdue += parseFloat(results[i].end_between_amount_pending);
             if (results[i].start_overdue_index !== "N/A") {
                 startOdIndex += parseFloat(results[i].start_overdue_index);
             }
@@ -37,10 +41,16 @@ export default ({ results, loading, startdate, enddate }) => {
         let collectionPercent = (totalPaid / (totalOverdue + totalPaid)) * 100;
         let odIndexVariation = endOdIndex - startOdIndex;
         let overdueVariation = totalEndOverdue - totalStartOverdue;
+        let betweenOverdueVariation = totalEndBetweenOverdue - totalStartBetweenOverdue;
+        let betweenCollectionPercent = (Math.abs(betweenOverdueVariation) * 100) / totalStartBetweenOverdue;
 
         totalOverdue = +totalOverdue.toFixed(2);
         totalStartOverdue = +totalStartOverdue.toFixed(2);
         totalEndOverdue = +totalEndOverdue.toFixed(2);
+        totalStartBetweenOverdue = +totalStartBetweenOverdue.toFixed(2);
+        totalEndBetweenOverdue = +totalEndBetweenOverdue.toFixed(2);
+        betweenOverdueVariation = +betweenOverdueVariation.toFixed(2);
+        betweenCollectionPercent = +betweenCollectionPercent.toFixed(2);
         overdueVariation = +overdueVariation.toFixed(2);
         startOdIndex = +startOdIndex.toFixed(2);
         odIndexVariation = odIndexVariation.toFixed(2);
@@ -57,6 +67,10 @@ export default ({ results, loading, startdate, enddate }) => {
                 totalOverdue,
                 totalStartOverdue,
                 totalEndOverdue,
+                totalStartBetweenOverdue,
+                totalEndBetweenOverdue,
+                betweenOverdueVariation,
+                betweenCollectionPercent,
                 overdueVariation,
                 startOdIndex,
                 endOdIndex,
@@ -102,6 +116,22 @@ export default ({ results, loading, startdate, enddate }) => {
                             <tr>
                                 <td>Overdue Variation</td>
                                 <td>{summary.overdueVariation == 0 ? <><font color="blue">{summary.overdueVariation.toLocaleString()}</font></> : <>{summary.overdueVariation > 0 ? <><font color="red">{'+'}{summary.overdueVariation.toLocaleString()}</font></> : <>{summary.overdueVariation === "N/A" ? <><font color="green">{summary.overdueVariation.toLocaleString()}</font></> : <><font color="green">{summary.overdueVariation.toLocaleString()}</font></>}</>}</>}</td>
+                            </tr>
+                            <tr>
+                                <td>Start Between Overdue</td>
+                                <td>{summary.totalStartBetweenOverdue > 0 ? (<Badge color="danger">{summary.totalStartBetweenOverdue.toLocaleString()}</Badge>) : (<Badge color="success">0</Badge>)}</td>
+                            </tr>
+                            <tr>
+                                <td>End Between Overdue</td>
+                                <td>{summary.totalEndBetweenOverdue > 0 ? (<Badge color="danger">{summary.totalEndBetweenOverdue.toLocaleString()}</Badge>) : (<Badge color="success">0</Badge>)}</td>
+                            </tr>
+                            <tr>
+                                <td>Between Overdue Variation</td>
+                                <td>{summary.betweenOverdueVariation == 0 ? <><font color="red">{summary.betweenOverdueVariation.toLocaleString()}</font></> : <>{summary.betweenOverdueVariation > 0 ? <><font color="red">{'+'}{summary.betweenOverdueVariation.toLocaleString()}</font></> : <>{summary.overdueVariation === "N/A" ? <><font color="green">{summary.betweenOverdueVariation.toLocaleString()}</font></> : <><font color="green">{summary.betweenOverdueVariation.toLocaleString()}</font></>}</>}</>}</td>
+                            </tr>
+                            <tr>
+                                <td>Between Collection Ratio</td>
+                                <td><Badge color="primary">{summary.betweenCollectionPercent}%</Badge></td>
                             </tr>
                             <tr>
                                 <td>Start Od Index</td>
