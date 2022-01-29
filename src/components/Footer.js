5ac21d96075e79a77ca21ccaa145e28784651f2a
Shamal Sandeep <ssdh.gdrive@gmail.com>
2022-01-29 16:35:18 +0530

    Updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ class Footer extends Component {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-12">
-                            2020 &copy; Agrivest (Private) Limited.
+                            2022 &copy; Agrivest (Private) Limited.
                         </div>
                     </div>
                 </div>
